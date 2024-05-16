352b4f07be8c4596e9aaee5ac9cacb49bece4b5d
Kywlyh Freazer <125483117+uchihokay@users.noreply.github.com>
2024-05-16 16:00:23 +0700

    Add files via upload

---
+++ 007 Check Carmichael_Number.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath> 
+using namespace std ;
+
+bool PrimeCheck(int x){
+    int var = 0;
+    for (int i = 1 ; i<=x ; i++){
+        if (x % i == 0){
+            var++;
+        }
+    }
+    if (var==2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int UCLN(int a , int b){
+    int c = 1;
+    while (c!=0){
+        c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;  
+}
+bool CarmichealCheck(int x, int n){
+    int t = n - 1;
+    long long var = 1;
+    while (t > 0) {                                //"thuat toan luy thua nhi phan
+        if (t % 2 == 1) {
+            var = (var * x) % n;
+            t--;
+        } else {
+            x = ((long long)x * x) % n;
+            t /= 2;
+        }                                           //"
+    }
+    cout << var << " ";
+    if (var == 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main(){
+    int n ; 
+    cout<<"n = ";cin>>n;
+    if (PrimeCheck(n) == false){
+        for (int i = 2 ; i < n ; i++){
+            if (UCLN(i,n) == 1){
+                if (CarmichealCheck(i,n)==true){
+                    cout<<"True";
+                }
+                else{
+                    cout<<"False";
+                }
+                break;
+            }
+        }
+    }
+    else{
+        cout<<"It's prime number.";
+    }
+}
