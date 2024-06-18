efe0a32cf281feea6843afc4d5905938b390d599
Kywlyh Freazer <125483117+uchihokay@users.noreply.github.com>
2024-06-19 00:12:53 +0700

    change all
    i completed this code with Eratosthenes and replaced array with vector , so my code can run faster .

--- 002 Check Carmichael_Number.cpp
+++ 002 Check Carmichael_Number.cpp
@@ -1,23 +1,29 @@
+// Viết chương trình tìm các số  Carmichael (là các số giả nguyên tố n thoả mãn
+//  điều kiện là hợp số và thoả mãn  � � −1 ≡ 1 (��� � ) với mọi số nguyên 
+// dương b nguyên tố cùng nhau với n) nhỏ hơn một số N cho trước nhập vào từ 
+// bàn phím (với điều kiện  0 ≤ � ≤ 10000.
 #include <iostream>
 #include <cmath> 
+#include <vector>
 using namespace std ;
+vector<int>primes;
 
-bool PrimeCheck(int x){
-    int var = 0;
-    for (int i = 1 ; i<=x ; i++){
-        if (x % i == 0){
-            var++;
+bool Eratosthenes(int n ){
+    vector<bool> isprime(n,true);
+    isprime[0] = isprime[1] = false;
+    for (int i = 2 ; i * i < n ; i++){
+        for (int j = i*i ; j < n ; j+=i){
+            isprime[j] = false;
         }
     }
-    if (var==2){
-        return true;
-    }
-    else{
-        return false;
+    for (int i = 0 ; i < n ; i++){
+        if (isprime[i] == true){
+            primes.push_back(i);
+        }
     }
 }
 
-int UCLN(int a , int b){
+int GCD(int a , int b){
     int c = 1;
     while (c!=0){
         c = a % b;
@@ -26,43 +32,44 @@ int UCLN(int a , int b){
     }
     return a;  
 }
-bool CarmichealCheck(int x, int n){
-    int t = n - 1;
-    long long var = 1;
-    while (t > 0) {                                //"thuat toan luy thua nhi phan
-        if (t % 2 == 1) {
-            var = (var * x) % n;
-            t--;
-        } else {
-            x = ((long long)x * x) % n;
-            t /= 2;
-        }                                           //"
+
+bool Carmichael(int n){
+    long long result, a , t;
+    for (int i = 0 ; i < primes.size() ; i++){
+        if (primes[i] == n){
+            return false;
+        }
     }
-    cout << var << " ";
-    if (var == 1){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-int main(){
-    int n ; 
-    cout<<"n = ";cin>>n;
-    if (PrimeCheck(n) == false){
-        for (int i = 2 ; i < n ; i++){
-            if (UCLN(i,n) == 1){
-                if (CarmichealCheck(i,n)==true){
-                    cout<<"True";
+    for (int i = 2 ; i < n ; i++){
+        a = i ; 
+        t = n - 1;
+        result = 1;
+        if (GCD(a,n) == 1){
+            while(t!=0){
+                if (t % 2 == 0){
+                    t = t/2;
+                    a = (a * a) % n;
                 }
                 else{
-                    cout<<"False";
+                    t --;
+                    result = (result * a) % n;
                 }
-                break;
+            }
+            if (result != 1){
+                return false;
             }
         }
     }
-    else{
-        cout<<"It's prime number.";
+    return true;
+}
+
+int main(){
+    int n ; 
+    cout<<"n = ";cin>>n;
+    Eratosthenes(n);
+    for (int i = 2 ; i < n ; i++){
+        if (Carmichael(i) == true){
+            cout<<i<<" ";
+        }
     }
 }
